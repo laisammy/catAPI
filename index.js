164960c04f5aa14315172540ac9ba73afba33d38
hei hei OK OK! <98307235+laisammy@users.noreply.github.com>
2026-02-13 01:08:24 +0000

    Added the ability to generate random cat pictures

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const catImgElem = document.getElementById("cat-img")
-
+document.addEventListener('DOMContentLoaded', setRandomCatImage)
+async function setRandomCatImage() {
+	const catapi_call = await fetch('https://api.thecatapi.com/v1/images/search')
+	const catapi_json = await catapi_call.json()
+	const img_url = catapi_json[0].url
+	const img_width = catapi_json[0].width
+	const img_height = catapi_json[0].height
+	const image_elem = document.getElementById('cat-img')
+	image_elem.src = img_url
+}
